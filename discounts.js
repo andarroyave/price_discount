008fd3373a187560c0ab1d20bf832681c62afd74
andarroyave <andarroyave@gmail.com>
2022-02-04 22:51:17 -0500

    se agregan estilos y función para mostrar resultado

--- discounts.js
+++ discounts.js
@@ -8,4 +8,13 @@ function calculateDiscountPrice(price, discount){
 
 }
 
-console.log(discountPrice);
+
+function getDiscount(){
+
+    const price = document.getElementById("price_input").value;
+    const discount = document.getElementById("discount_input").value;
+    const finalPrice = calculateDiscountPrice(price, discount);
+    console.log(finalPrice)
+    document.getElementById("result_message").innerText = "El precio final es $" + finalPrice
+
+}
