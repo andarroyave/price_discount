3ccb4d3f69880db69fc1b650db2d81d34954814e
andarroyave <andarroyave@gmail.com>
2022-02-06 20:17:49 -0500

    se agrega soporte de cupones

--- discounts.js
+++ discounts.js
@@ -1,6 +1,22 @@
 const price = 120;
 const discount = 18;
 
+const coupons = [
+    {
+        name: "cupon1",
+        discount: 10,
+    },
+    {
+        name: "cupon2",
+        discount: 20,
+    },
+    {
+        name: "cupon3",
+        discount: 30,
+    }
+]
+
+
 
 function calculateDiscountPrice(price, discount){
     const discountPrice = price - price*discount/100;
@@ -8,13 +24,30 @@ function calculateDiscountPrice(price, discount){
 
 }
 
+function getCoupon(name){    
+    let coupon = coupons.find(coupon => coupon.name==name)  
+    return coupon
+  
+}
+
+function validateCoupon(name){
+    return getCoupon(name) != undefined
+
+}
 
 function getDiscount(){
 
-    const price = document.getElementById("price_input").value;
-    const discount = document.getElementById("discount_input").value;
-    const finalPrice = calculateDiscountPrice(price, discount);
-    console.log(finalPrice)
-    document.getElementById("result_message").innerText = "El precio final es $" + finalPrice
+    let couponName = document.getElementById("coupon_input").value;
+    if(!validateCoupon(couponName)){
+        alert("El cupón no es válido.")
+    }
+    else{
+        const price = document.getElementById("price_input").value;
+        const discount = getCoupon(couponName).discount;
+        const finalPrice = calculateDiscountPrice(price, discount);
+        console.log(finalPrice)
+        document.getElementById("result_message").innerText = "El precio final es $" + finalPrice
+    }
+
 
 }
